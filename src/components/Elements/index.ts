d478c704a569d96350ef6c229740a2f5a63a34fc
Gambitier <csegambitie@gmail.com>
2024-01-02 03:11:27 +0530

    add wrappers to more mui components

--- src/components/Elements/index.ts
+++ src/components/Elements/index.ts
@@ -1,2 +1,7 @@
+export * from './Box';
 export * from './Button';
+export * from './Container';
+export * from './CssBaseline';
 export * from './Link';
+export * from './Paper';
+export * from './Typography';
