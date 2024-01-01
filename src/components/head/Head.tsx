5088653d8ad8ec31a9431a4fac2499e63e4c74f9
Gambitier <csegambitie@gmail.com>
2024-01-02 02:33:24 +0530

    use app name

--- src/components/head/Head.tsx
+++ src/components/head/Head.tsx
@@ -1,3 +1,4 @@
+import { APP_NAME } from '@/config';
 import { Helmet } from 'react-helmet-async';
 
 type HeadProps = {
@@ -8,8 +9,8 @@ type HeadProps = {
 export const Head = ({ title = '', description = '' }: HeadProps = {}) => {
   return (
     <Helmet
-      title={title ? `${title} | Bulletproof React` : undefined}
-      defaultTitle="Bulletproof React"
+      title={title ? `${title} | ${APP_NAME}` : undefined}
+      defaultTitle={`${APP_NAME}`}
     >
       <meta name="description" content={description} />
     </Helmet>
