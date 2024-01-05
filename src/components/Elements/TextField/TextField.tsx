98e18744d25ad4f64eba1c57fd5a5bc2bf21ff3f
Gambitier <csegambitie@gmail.com>
2024-01-05 18:16:39 +0530

    fix error - Function components cannot be given refs. Attempts to access this ref will fail. Did you mean to use React.forwardRef()

--- src/components/Elements/TextField/TextField.tsx
+++ src/components/Elements/TextField/TextField.tsx
@@ -1,13 +1,19 @@
 import {
-    TextField as MUITextField,
-    TextFieldProps as MUITextFieldProps,
-  } from '@mui/material';
-  
-  export type TextFieldProps = MUITextFieldProps & {
-    // Add any additional props specific to CustomTypography
-  };
-  
-  export const TextField = ({ children, ...props }: TextFieldProps) => {
-    return <MUITextField {...props}>{children}</MUITextField>;
-  };
-  
+  TextField as MUITextField,
+  TextFieldProps as MUITextFieldProps,
+} from '@mui/material';
+import React from 'react';
+
+export type TextFieldProps = MUITextFieldProps & {
+  // Add any additional props specific to CustomTypography
+};
+
+export const TextField = React.forwardRef<HTMLDivElement, TextFieldProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <MUITextField {...props} ref={ref}>
+        {children}
+      </MUITextField>
+    );
+  },
+);
