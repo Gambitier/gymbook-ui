6d3bae09624c4901cd31e21c8f76c4f27b839ccf
AJ5154 <ashishjadhav597@gmail.com>
2024-01-06 12:13:50 +0530

    Add a plan component and start creating this component

---
+++ src/features/Components/Plan.tsx
@@ -0,0 +1,16 @@
+import { Button, Typography } from '@/components/Elements';
+
+const Plan = () => {
+  return (
+    <div>
+        <Typography component= 'h1' variant='h6' >
+            Add Plan
+        </Typography>
+        <Button variant='contained'>
+            Add Plan
+        </Button>
+    </div>
+  )
+}
+
+export default Plan
