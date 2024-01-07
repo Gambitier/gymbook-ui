ba4cb3d361abf2e47a0ccf15a0fd7063ec4d221c
AJ5154 <ashishjadhav597@gmail.com>
2024-01-07 13:27:46 +0530

    add Sidebar and shows plan in side bar

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -5,17 +5,40 @@ type SideNavigationItem = {
   to: string;
 };
 
+const SidebarNavigation = () => {
+  const navigation = [{ name: 'Plan', to: 'plans' }].filter(
+    Boolean,
+  ) as SideNavigationItem[];
+  return (
+    <>
+      {navigation.map((item, index) => (
+        <NavLink end={index === 0} key={item.name} to={item.to}>
+          {item.name}
+        </NavLink>
+      ))}
+    </>
+  );
+};
+
+const Sidebar = () => {
+  return (
+    <div className="container">
+      <div className="container_sidenav">
+        <SidebarNavigation />
+      </div>
+    </div>
+  );
+};
+
 type MainLayoutProps = {
   children: React.ReactNode;
 };
 
 const MainLayout = ({ children }: MainLayoutProps) => {
-  const navigation: SideNavigationItem[] = [{ name: 'Plan', to: './plan' }];
   return (
     <div>
-      {navigation.map((item, index) => (
-        <NavLink end={index === 0} key={item.name} to={item.to}></NavLink>
-      ))}
+      <Sidebar />
+
       <main>{children}</main>
     </div>
   );
--- src/features/auth/routes/Signup.tsx
+++ src/features/auth/routes/Signup.tsx
@@ -7,7 +7,7 @@ const Signup = () => {
 
   return (
     <Layout title="Sign up to your account">
-      <SignupForm onSuccess={() => navigate('/app/plans')} />
+      <SignupForm onSuccess={() => navigate('/app')} />
     </Layout>
   );
 };
