5ffcd27094ee1d9554cb79dba68786655188df0b
AJ5154 <ashishjadhav597@gmail.com>
2024-01-07 13:52:03 +0530

    filter is removed

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,34 +1,4 @@
-import { NavLink } from 'react-router-dom';
-
-type SideNavigationItem = {
-  name: string;
-  to: string;
-};
-
-const SidebarNavigation = () => {
-  const navigation = [{ name: 'Plan', to: 'plans' }].filter(
-    Boolean,
-  ) as SideNavigationItem[];
-  return (
-    <>
-      {navigation.map((item, index) => (
-        <NavLink end={index === 0} key={item.name} to={item.to}>
-          {item.name}
-        </NavLink>
-      ))}
-    </>
-  );
-};
-
-const Sidebar = () => {
-  return (
-    <div className="container">
-      <div className="container_sidenav">
-        <SidebarNavigation />
-      </div>
-    </div>
-  );
-};
+import { Sidebar } from './Sidebar';
 
 type MainLayoutProps = {
   children: React.ReactNode;
@@ -38,7 +8,6 @@ const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div>
       <Sidebar />
-
       <main>{children}</main>
     </div>
   );
---
+++ src/components/Layout/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { NavLink } from 'react-router-dom';
+
+type SideNavigationItem = {
+  name: string;
+  to: string;
+};
+const SidebarNavigation = () => {
+  const navigation:SideNavigationItem[] = [{ name: 'Plan', to: 'plans' }]
+  return (
+    <>
+      {navigation.map((item, index) => (
+        <NavLink end={index === 0} key={item.name} to={item.to}>
+          {item.name}
+        </NavLink>
+      ))}
+    </>
+  );
+};
+export const Sidebar = () => {
+  return (
+    <div className="container">
+      <div className="container_sidenav">
+        <SidebarNavigation />
+      </div>
+    </div>
+  );
+};
