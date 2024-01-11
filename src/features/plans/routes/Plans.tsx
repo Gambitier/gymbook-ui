d00524499a0ca9b2a97da8039cb3232a4cc39826
AJ5154 <ashishjadhav597@gmail.com>
2024-01-11 22:48:49 +0530

    remove className

--- src/features/plans/routes/Plans.tsx
+++ src/features/plans/routes/Plans.tsx
@@ -4,7 +4,7 @@ import { CreatePlan } from '../Components/CreatePlan';
 const Plans = () => {
   return (
     <ContentLayout title="Plans">
-      <div className="plan_container">
+      <div>
         <CreatePlan />
       </div>
     </ContentLayout>
