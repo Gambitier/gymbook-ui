bd0eb2626bab864de266da9a9d06997d6c5e735a
Gambitier <csegambitie@gmail.com>
2024-01-14 20:59:40 +0530

    refactor types

--- src/features/plans/types/index.ts
+++ src/features/plans/types/index.ts
@@ -1,14 +1,1 @@
-import { PlanAPIResponse } from '@/types/api/planAPI';
-
-export type CreatePlanResponse = {
-  id: string;
-  name: string;
-  price: number;
-  durationInMoths: number;
-  createdAt: Date;
-  updatedAt: Date;
-  deleted: null;
-  gymId: string;
-};
-
-export type CreatePlanResponseDTO = PlanAPIResponse<CreatePlanResponse>;
+export * from './createPlan';
--- src/types/api/planAPI.ts
+++
@@ -1,7 +0,0 @@
-type PlanAPIResponseStatus = 'Suceess';
-
-export type PlanAPIResponse<T> = {
-  status: PlanAPIResponseStatus;
-  message: string;
-  data: T;
-};
