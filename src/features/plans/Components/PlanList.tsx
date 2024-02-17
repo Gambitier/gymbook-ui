e724e8da454649877ed67808e79e08559d4418ac
Gambitier <csegambitie@gmail.com>
2024-02-17 17:48:57 +0530

    Revert "fix error"
    This reverts commit c66678c53b20303eee92680d9e510c3c086ff152.

--- src/features/plans/Components/PlanList.tsx
+++ src/features/plans/Components/PlanList.tsx
@@ -1,13 +1,13 @@
 import Table from '@/components/Elements/Table/Table';
-import { CreatePlanResponse } from '@/features/plans';
 import { Box, Button, CircularProgress, Typography } from '@mui/material';
 import { ColumnDef } from '@tanstack/react-table';
 import { useState } from 'react';
+import { dummyData } from './DummyData';
 import { usePlan } from '../api/getPlan';
 export const PlanList = () => {
   const apiData = usePlan();
 
-  const data = apiData.data?.();
+  const data = dummyData;
   const [currentPage, setCurrentPage] = useState<number | undefined>(1);
   const [searchItem, setSearchItem] = useState<string | undefined>('');
 
@@ -19,73 +19,60 @@ export const PlanList = () => {
     );
   }
 
-  if (!data) return null;
+  if (!apiData.data) return null;
 
   const itemsPerPage = 5;
 
-  async function filterDataAsync(): Promise<CreatePlanResponse[]> {
-    const resolvedData = await data;
+  const filteredData = data.filter((item) =>
+    Object.values(item).some(
+      (value) =>
+        typeof value === 'string' &&
+        value.toLowerCase().includes((searchItem ?? '').toLowerCase()),
+    ),
+  );
 
-    if (resolvedData === undefined) {
-      return [];
-    }
+  const totalItems = filteredData.length;
+  const pageCount = Math.ceil(totalItems / itemsPerPage);
 
-    const filteredData = resolvedData.filter((item: CreatePlanResponse) =>
-      Object.values(item).some(
-        (value) =>
-          typeof value === 'string' &&
-          value.toLowerCase().includes((searchItem ?? '').toLowerCase()),
-      ),
-    );
+  const startIndex = ((currentPage ?? 1) - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
 
-    return filteredData;
-  }
+  const currentData = filteredData.slice(startIndex, endIndex);
 
-  filterDataAsync().then((filteredData) => {
-    const totalItems = filteredData.length;
-    const pageCount = Math.ceil(totalItems / itemsPerPage);
+  const ColumnData: ColumnDef<unknown, unknown>[] = [
+    { accessorKey: 'id', header: 'ID' },
+    { accessorKey: 'first_name', header: 'First Name' },
+    { accessorKey: 'last_name', header: 'Last Name' },
+    { accessorKey: 'email', header: 'Email' },
+    { accessorKey: 'phone_number', header: 'Phone Number' },
+    { accessorKey: 'date_of_birth', header: 'Date of Birth' },
+  ];
 
-    const startIndex = ((currentPage ?? 1) - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
+  const Header = (
+    <Box display="flex" justifyContent="space-between">
+      <Typography variant="h4" alignItems="center">
+        User Table
+      </Typography>
+      <Button>Action Button</Button>
+    </Box>
+  );
 
-    const currentData = filteredData.slice(startIndex, endIndex);
-    console.log(filteredData);
-
-    const ColumnData: ColumnDef<unknown, unknown>[] = [
-      { accessorKey: 'id', header: 'ID' },
-      { accessorKey: 'first_name', header: 'First Name' },
-      { accessorKey: 'last_name', header: 'Last Name' },
-      { accessorKey: 'email', header: 'Email' },
-      { accessorKey: 'phone_number', header: 'Phone Number' },
-      { accessorKey: 'date_of_birth', header: 'Date of Birth' },
-    ];
-
-    const Header = (
-      <Box display="flex" justifyContent="space-between">
-        <Typography variant="h4" alignItems="center">
-          User Table
-        </Typography>
-        <Button>Action Button</Button>
-      </Box>
-    );
-
-    return (
-      <div>
-        <Table
-          data={currentData}
-          columns={ColumnData}
-          isFetching={false}
-          headerComponent={Header}
-          onClickRow={(_, row) => {
-            console.log(row);
-          }}
-          pageCount={pageCount}
-          page={setCurrentPage}
-          search={setSearchItem}
-          skeletonCount={3}
-          skeletonHeight={50}
-        />
-      </div>
-    );
-  });
+  return (
+    <div>
+      <Table
+        data={currentData}
+        columns={ColumnData}
+        isFetching={false}
+        headerComponent={Header}
+        onClickRow={(_, row) => {
+          console.log(row);
+        }}
+        pageCount={pageCount}
+        page={setCurrentPage}
+        search={setSearchItem}
+        skeletonCount={3}
+        skeletonHeight={50}
+      />
+    </div>
+  );
 };
