e72d5284ccacfe66d4ea552d995683e740e19d73
ashishreacts <ashishjadhav597@gmail.com>
2024-03-01 14:22:04 +0530

    create gym list

---
+++ src/features/createGym/components/GymList.tsx
@@ -0,0 +1,74 @@
+import { Box, Button, Typography } from '@/components/Elements';
+import Table from '@/components/Elements/Table/Table';
+import { GetGymResponseDTO } from '@/features/createGym';
+import { useAllGym } from '../api/getAllGym';
+import { ColumnDef } from '@tanstack/react-table';
+import { useState } from 'react';
+import { CircularProgress } from '@mui/material';
+
+export const GymList = () => {
+  const getGymQuery = useAllGym();
+
+  const [currentPage, setCurrentPage] = useState<number | undefined>(1);
+  const [searchItem, setSearchItem] = useState<string | undefined>('');
+
+  if (getGymQuery.isLoading) {
+    return (
+      <>
+        <CircularProgress size="lg" />
+      </>
+    );
+  }
+
+  const data = getGymQuery.data;
+  if (!data) return null;
+  console.log('value:', data);
+
+  const itemsPerPage = 5;
+
+  // search functionality
+  const filteredData = data.data.filter((item) =>
+    Object.values(item).some(
+      (value) =>
+        typeof value === 'string' &&
+        value.toLowerCase().includes((searchItem ?? '').toLowerCase()),
+    ),
+  );
+
+  // pagination functionality
+  const totalItems = filteredData.length;
+  const pageCount = Math.ceil(totalItems / itemsPerPage);
+  const startIndex = ((currentPage ?? 1) - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const paginatedData = filteredData.slice(startIndex, endIndex);
+  const columnDef: ColumnDef<GetGymResponseDTO, unknown>[] = [
+    { accessorKey: 'id', header: 'ID' },
+    { accessorKey: 'name', header: ' Name' },
+  ];
+  const Header = (
+    <Box display="flex" justifyContent="space-between">
+      <Typography variant="h4" alignItems="center">
+        User Table
+      </Typography>
+      <Button>Action Button</Button>
+    </Box>
+  );
+  return (
+    <div>
+      <Table
+        data={paginatedData}
+        columns={columnDef}
+        isFetching={false}
+        headerComponent={Header}
+        onClickRow={(_, row) => {
+          console.log(row);
+        }}
+        pageCount={pageCount}
+        page={setCurrentPage}
+        search={setSearchItem}
+        skeletonCount={3}
+        skeletonHeight={50}
+      />
+    </div>
+  );
+};
--- src/features/createGym/routes/Gym.tsx
+++ src/features/createGym/routes/Gym.tsx
@@ -1,11 +1,15 @@
 import { ContentLayout } from '@/components/Layout';
 import { CreateGym } from '../components/CreateGym';
+import { GymList } from '../components/GymList';
 
 const Gym = () => {
   return (
     <ContentLayout title="Create Gym">
       <div>
         <CreateGym />
+      </div>
+      <div>
+        <GymList />
       </div>
     </ContentLayout>
   );
